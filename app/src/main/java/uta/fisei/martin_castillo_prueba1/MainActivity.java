1bf1c8671ec01f0d58a000bd1fd77e1a142f7c5d
Martin Castillo <mcstv799@gmail.com>
2023-10-24 17:17:13 -0500

    Martin Castillo 17h15

---
+++ app/src/main/java/uta/fisei/martin_castillo_prueba1/MainActivity.java
@@ -0,0 +1,27 @@
+package uta.fisei.martin_castillo_prueba1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonMostrarResultados;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonMostrarResultados = findViewById(R.id.)
+    }
+
+    public void onClickSecondActivity(View view){
+        Intent intent = new Intent(this,SecondActivity.class);
+        startActivity(intent);
+    }
+
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Martin_Castillo_prueba1"
+include(":app")
